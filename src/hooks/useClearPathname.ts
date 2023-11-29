7695a95ffa0c19c3ee0b2eaa19354d52c80b3726
Ruslan Volovenko <vokoloven@gmail.com>
2023-11-29 10:51:00 +0200

    chore: Updated hook useClearPathname

--- src/hooks/useClearPathname.ts
+++ src/hooks/useClearPathname.ts
@@ -6,9 +6,12 @@ export const useClearPathname = () => {
   const pathname = usePathname();
 
   const clearPathname = pathname.replace(/\//, "");
+  const nestedUrl = clearPathname.match(/(.+?)\//);
 
   if (pathname === "/") {
     return "/";
+  } else if (nestedUrl) {
+    return nestedUrl[1];
   } else {
     return clearPathname;
   }
