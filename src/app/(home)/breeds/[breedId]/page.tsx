068c4524527cd02290400bcb5e0e8f0e9c21bc4a
Ruslan Volovenko <vokoloven@gmail.com>
2023-11-29 10:26:27 +0200

    style: Added Screen query for [breedId] page

--- src/app/(home)/breeds/[breedId]/page.tsx
+++ src/app/(home)/breeds/[breedId]/page.tsx
@@ -19,7 +19,7 @@ export default function SelectedBreed({
   return (
     <Backtab
       boxElement={
-        <div className="ml-2.5 px-[30px] py-[5px] bg-darkPink rounded-1.5lg text-white font-medium text-xl leading-[30px]">
+        <div className="ml-2.5 mobile:px-4 px-[30px] py-[5px] bg-darkPink rounded-1.5lg text-white font-medium text-xl leading-[30px] tracking-[2px]">
           {breedId.toUpperCase()}
         </div>
       }
@@ -32,12 +32,12 @@ export default function SelectedBreed({
       {Boolean(images?.length) && (
         <div className="mt-5 flex flex-col">
           <CustomSlider images={images} />
-          <div className="relative bg-white rounded-2.5xl px-10 py-10 w-full border-2 border-lightPink border-solid mt-[50px]">
+          <div className="relative bg-white rounded-2.5xl p-10 mobile:p-5 w-full border-2 border-lightPink border-solid mt-[50px]">
             <div className="flex justify-center flex-col">
-              <h2 className="font-medium text-xl text-placeholder text-center">
+              <h2 className="font-medium text-xl text-placeholder text-center mobile:text-base">
                 Breed description
               </h2>
-              <div className="mt-5 flex">
+              <div className="mt-5 flex mobile:flex-col mobile:mt-0">
                 <div className="w-full">
                   <ul>
                     <li className="text-black font-medium">Temperament:</li>
@@ -46,7 +46,7 @@ export default function SelectedBreed({
                     </li>
                   </ul>
                 </div>
-                <div className="w-full ml-5">
+                <div className="w-full ml-5 mobile:ml-0">
                   <ul>
                     {Boolean(images?.length) &&
                       breedIdItem(images?.[0])!.map((item) => (
@@ -63,7 +63,7 @@ export default function SelectedBreed({
                 </div>
               </div>
             </div>
-            <div className="whitespace-nowrap rounded-2.5xl bg-white py-[5px] px-10 font-medium text-black text-4xl absolute top-0 left-[50%] -translate-y-1/2 -translate-x-1/2 leading-normal text-center">
+            <div className="mobile:text-xl mobile:px-5 mobile:py-4 whitespace-nowrap rounded-2.5xl bg-white py-[5px] px-10 font-medium text-black text-4xl absolute top-0 left-[50%] -translate-y-1/2 -translate-x-1/2 leading-normal text-center">
               {images?.[0].breeds[0]?.name}
             </div>
           </div>
